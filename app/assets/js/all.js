5b81097592d92292d0c2fc37f980dbbe49bdba58
ShaniMaCC <ning830315@gmail.com>
2021-08-06 20:26:04 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.expand').click(function(){
+    if($('.expand__less').is('.d-none')){
+      $('.expand__more').addClass('d-none');
+      $('.expand__less').removeClass('d-none');
+    }
+    else{
+      $('.expand__more').removeClass('d-none');
+      $('.expand__less').addClass('d-none')
+    }
+  })
+})
